1199d89d7f6944862307c4de0bf9b3631f45d57a
alex palatov <sonnerammstein1989@gmail.com>
2024-06-19 18:16:19 +0300

    Part#3. Stream

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class Main {
-    public static void main(String[] args) {
 
+    static final String input = new String(
+            "Привет, Александр, с праздником, с Новым Годом");
+
+    public static void main(String[] args){
+
+        List<String> tmp = Stream.of(String.valueOf(" "+input).split(" ")
+        ).map(String::toUpperCase).collect(Collectors.toList());
+
+        String res = tmp.stream().reduce(
+        (out, x)->{
+            if(x.startsWith("С"))   return out.concat(" "+x);
+            if(x.startsWith("ПРА")) return out.concat(" "+x);
+                String start = String.valueOf(x.charAt(0)).toLowerCase();
+                String end = String.copyValueOf(x.toCharArray(), 1, x.length() - 1);
+                return out.concat(" " + start + end);
+        }).get();
+
+        System.out.println(res);
     }
 }
--- src/lessImpl.java
+++ src/lessImpl.java
@@ -29,8 +29,8 @@ public class lessImpl {
 
             String a = new String("first");
             String b = new String("second");
-            printTwo(a,b , Main::concatImplA);
-            printTwo(a,b , Main::concatImplB);
+            printTwo(a,b , Impl::concatImplA);
+            printTwo(a,b , Impl::concatImplB);
         }
         static void printTwo(String a, String b, Concatable c){System.out.println(c.concat(a,b));}
         static String concatImplA(String a, String b){return new String(a+","+b);}
